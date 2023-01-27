14be7496dd97167b0b4d8060412ce234426cebcc
gloghry <loghry.g@gmail.com>
2023-01-26 19:44:54 -0800

    lab1 updated code

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -15,7 +15,7 @@
 #endif
 
 #define LED_PIN 25
-#define GPIO_18 19
+#define GPIO_18 18
 #define SW1 17
 #define SW2 16
 
@@ -35,6 +35,7 @@ void my_gpio_init(void) {
     gpio_init(SW2);
     gpio_set_dir(SW2, GPIO_IN);
     gpio_pull_up(SW2);
+    /* end of new code added to init */
 }
 
 void led_control(bool isOn) {
@@ -76,9 +77,9 @@ int main() {
 
                 /* Want 15Hz blink, best approx */
                 led_control(1);
-                sleep_ms(7);
+                sleep_ms(7);                // 100/15 rounded
                 led_control(0);
-                sleep_ms(60);
+                sleep_ms(60);               // 900/15
             }
             /* Weird logic, if switch 1 held and 
              * switch 2 not pressed do nothing.
@@ -89,6 +90,7 @@ int main() {
              */
             while (isPressed(SW1) && !isPressed(SW2));
         }
+
         if (isPressed(SW2)) {
             for (int i = 0; i < 10; i++) {
                 /* UNKONWN REQUIREMENT
@@ -99,9 +101,9 @@ int main() {
 
                 /* Want 13Hz blink, best approx */
                 led_control(1);
-                sleep_ms(8);
+                sleep_ms(8);                // 100/13 rounded
                 led_control(0);
-                sleep_ms(69);
+                sleep_ms(69);               // 900/13
             }
             /* Weird logic, if switch 2 held and 
              * switch 1 not pressed do nothing.
@@ -112,12 +114,21 @@ int main() {
              */
             while (isPressed(SW2) && !isPressed(SW1));
         }   
+
         while (isPressed(SW1) && isPressed(SW2)) {
-            /* Want 5Hz blink */
+            /* Want 5Hz blink when both switches pressed */
             led_control(1);
             sleep_ms(20);
             led_control(0);
             sleep_ms(180);
+        }
+        
+        while (!isPressed(SW1) && !isPressed(SW2)) {
+            /* Want 1Hz blink when no switches pressed */
+            led_control(1);
+            sleep_ms(5);
+            led_control(0);
+            sleep_ms(995);
         }
 #endif
     }
